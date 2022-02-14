318d5d747aef0d0e5a37dd0a5b77515234f3f406
zerrium <zerrium@mail.com>
2022-02-14 12:48:13 +0700

    Added nether roof map

---
+++ zen4/nether_roof/custom.markers.js
@@ -0,0 +1,167 @@
+
+UnminedCustomMarkers = {
+
+    isEnabled: true,
+
+    markers: [
+        {
+            x: -19,
+            z: -4,
+            image: "location-pin.png",
+            imageAnchor: [0.2, 1],
+            imageScale: 0.2,
+            text: "Spawn Portal",
+            textColor: "white", 
+            offsetX: 0,
+            offsetY: 10,
+            font: "bold 14px Calibri,sans serif",
+        },
+
+        {
+            x: -93,
+            z: 110,
+            image: "location-pin.png",
+            imageAnchor: [0.2, 1],
+            imageScale: 0.2,
+            text: "Shopping District",
+            textColor: "white", 
+            offsetX: 0,
+            offsetY: 10,
+            font: "bold 14px Calibri,sans serif",
+        },
+
+        {
+            x: -100,
+            z: -168,
+            image: "location-pin.png",
+            imageAnchor: [0.2, 1],
+            imageScale: 0.2,
+            text: "Stronghold",
+            textColor: "white", 
+            offsetX: 0,
+            offsetY: 10,
+            font: "bold 14px Calibri,sans serif",
+        },
+
+        {
+            x: 188,
+            z: 189,
+            image: "location-pin.png",
+            imageAnchor: [0.2, 1],
+            imageScale: 0.2,
+            text: "Desert",
+            textColor: "white", 
+            offsetX: 0,
+            offsetY: 10,
+            font: "bold 14px Calibri,sans serif",
+        },
+
+        {
+            x: -1552,
+            z: -1234,
+            image: "location-pin.png",
+            imageAnchor: [0.2, 1],
+            imageScale: 0.2,
+            text: "Woodland Mansion",
+            textColor: "white", 
+            offsetX: 0,
+            offsetY: 10,
+            font: "bold 14px Calibri,sans serif",
+        },
+
+        {
+            x: -812,
+            z: -400,
+            image: "location-pin.png",
+            imageAnchor: [0.2, 1],
+            imageScale: 0.2,
+            text: "???",
+            textColor: "white", 
+            offsetX: 0,
+            offsetY: 10,
+            font: "bold 14px Calibri,sans serif",
+        },
+
+        // --- Terra ---
+        {
+            x: -96,
+            z: -94,
+            image: "location-pin.png",
+            imageAnchor: [0.2, 1],
+            imageScale: 0.2,
+            text: "Terra Base",
+            textColor: "white", 
+            offsetX: 0,
+            offsetY: 10,
+            font: "bold 14px Calibri,sans serif",
+        },
+
+        {
+            x: -32,
+            z: 135,
+            image: "location-pin.png",
+            imageAnchor: [0.2, 1],
+            imageScale: 0.2,
+            text: "Terra Island",
+            textColor: "white", 
+            offsetX: 0,
+            offsetY: 10,
+            font: "bold 14px Calibri,sans serif",
+        },
+
+        {
+            x: 318,
+            z: 173,
+            image: "location-pin.png",
+            imageAnchor: [0.2, 1],
+            imageScale: 0.2,
+            text: "Terra Fortress",
+            textColor: "white", 
+            offsetX: 0,
+            offsetY: 10,
+            font: "bold 14px Calibri,sans serif",
+        },
+
+        // --- JourBlock ---
+        {
+            x: -246,
+            z: 74,
+            image: "location-pin.png",
+            imageAnchor: [0.2, 1],
+            imageScale: 0.2,
+            text: "JourBlock",
+            textColor: "white", 
+            offsetX: 0,
+            offsetY: 10,
+            font: "bold 14px Calibri,sans serif",
+        },
+
+        // --- Hexacorp ---
+        {
+            x: -326,
+            z: -337,
+            image: "location-pin.png",
+            imageAnchor: [0.2, 1],
+            imageScale: 0.2,
+            text: "Hexacorp HQ",
+            textColor: "white", 
+            offsetX: 0,
+            offsetY: 10,
+            font: "bold 14px Calibri,sans serif",
+        },
+
+        // --- YeetGenic ---
+        {
+            x: 195,
+            z: -153,
+            image: "location-pin.png",
+            imageAnchor: [0.2, 1],
+            imageScale: 0.2,
+            text: "YeetGenic",
+            textColor: "white", 
+            offsetX: 0,
+            offsetY: 10,
+            font: "bold 14px Calibri,sans serif",
+        },
+    ]
+}
